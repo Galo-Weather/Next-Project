9c704f9f6d37386ff41da143c81346415ae0f62d
Akiozin <rm98582@fiap.com.br>
2023-10-30 21:05:00 -0300

    Sobre nos feito

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -2,6 +2,12 @@ import styled from "styled-components";
 import React from 'react';
 import mainGif from '../../imgs/capaGif.gif';
 import problemaImagem from '../../imgs/problemaImagem.jpg';
+import Fabricio from '../../imgs/time/Fabricio.png'
+import GuilhermeA from '../../imgs/time/Guilherme A.png'
+import GuilhermeM from '../../imgs/time/Guilherme M.png'
+import Matheus from '../../imgs/time/Matheus.png'
+import Vinicius from '../../imgs/time/Vinicius.png'
+import Galoicon from '../../imgs/galoIcon.png'
 
 
 const Home = styled.section`
@@ -151,6 +157,73 @@ const CausaContent = styled.div`
     padding: 1em;
 }
 `
+const AboutContainer = styled.section`
+background: linear-gradient(#e8e8e8, #ffffff 15%);
+  text-align: center;
+  padding: 1em;
+  h1{
+    margin-bottom: 1em;
+  }
+
+  @media (min-width: 1100px) {
+    display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  h1{
+    margin-bottom: 1em;
+  }
+}
+`;
+
+
+const AboutImg = styled.div`
+img{
+    width: 100%;
+    padding: 1em;
+}
+`;
+
+const AboutTxT = styled.div`
+  max-width: 600px;
+  padding-top: 1em;
+`;
+
+const CardContainer = styled.div`
+display:flex;
+justify-content: center;
+align-items: center;
+margin-top: 50px;
+flex-wrap: wrap;
+`
+
+const Card = styled.div`
+width: 325px;
+background-color: #0f62fe;
+color: #fff;
+border-radius: 10px;
+box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
+overflow: hidden;
+margin: 10px;
+img{
+    width:100%;
+    height: 225px;
+    object-fit: cover;
+}
+`
+
+const CardContent = styled.div`
+padding: 20px;
+h2{
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+p{
+    font-size: 14px;
+    margin-bottom: 20px;
+}
+`
 
 function MainPage() {
     return (
@@ -203,6 +276,93 @@ function MainPage() {
                     <CausaContent>
                     </CausaContent>
                 </CausaContainer>
+            </>
+            <>
+            <AboutContainer>
+                <h1>Sobre nós</h1>
+                <Container>
+                    <AboutImg>
+                        <img
+                            src={Galoicon}
+                            alt="Icone da logo marca">
+                        </img>
+                    </AboutImg>
+                    <AboutTxT>
+                        <p> No coração do "Galo Weather" está nossa visão ousada de transformar radicalmente a forma como
+                            enfrentamos os desafios dos alagamentos urbanos. Inspirados pelo galo, símbolo tradicionalmente
+                            associado à previsão do tempo e à vigilância, buscamos antecipar e mitigar as inundações que impactam
+                            comunidades em áreas urbanas. Assim como o galo anuncia o amanhecer, queremos antever e preparar as cidades
+                            para lidar proativamente com as intempéries climáticas, integrando tecnologia inovadora e análise de dados para
+                            criar cidades mais seguras e resilientes.</p>
+
+                        <p>A escolha do galo como nosso símbolo vai além da previsão do tempo; ele representa nossa missão de despertar a
+                            consciência e ação preventiva diante das inundações. Assim como o galo é vigilante e anuncia mudanças climáticas,
+                            nós, no "Galo Weather", aspiramos a ser a voz que alerta e mobiliza comunidades para se prepararem e se protegerem.
+                            Nossa missão é utilizar a sabedoria da natureza, combinada com a inovação tecnológica, para forjar um futuro onde
+                            as cidades possam enfrentar os desafios climáticos, garantindo segurança e bem-estar para todos.</p>
+                    </AboutTxT>
+                </Container>
+            </AboutContainer>
+
+            <CardContainer>
+
+                <Card>
+                    <img
+                        src={Fabricio}
+                        alt="Integrante do grupo Galo">
+                    </img>
+                    <CardContent>
+                        <h2>Fabrício Saavedra</h2>
+                        <p>RM 97631</p>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <img
+                        src={GuilhermeA}
+                        alt="Integrante do grupo Galo">
+                    </img>
+                    <CardContent>
+                        <h2>Guilherme Akio</h2>
+                        <p>RM 98582</p>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <img
+                        src={GuilhermeM}
+                        alt="Integrante do grupo Galo">
+                    </img>
+                    <CardContent>
+                        <h2>Guilherme Morais</h2>
+                        <p>RM 551981</p>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <img
+                        src={Matheus}
+                        alt="Integrante do grupo Galo">
+                    </img>
+                    <CardContent>
+                        <h2>Matheus Motta</h2>
+                        <p>RM 550352</p>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <img
+                        src={Vinicius}
+                        alt="Integrante do grupo Galo">
+                    </img>
+                    <CardContent>
+                        <h2>Vinicius Buzato</h2>
+                        <p>RM 99125</p>
+                    </CardContent>
+                </Card>
+
+            </CardContainer>
+
             </>
         </>
     );
