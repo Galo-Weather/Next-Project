b3b17ab690b604ae7bd1e146623a8995abe49efa
Akiozin <rm98582@fiap.com.br>
2023-11-21 12:58:48 -0300

    Footer sass

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,50 +2,22 @@ import styled from "styled-components";
 import React from 'react';
 import logo from '../../imgs/galoBanner.png'
 import fiap from '../../imgs/FiapLogo.png'
-
-const FooterContainer = styled.div`
-    position: absolute;
-    height: auto;
-    width: 100%;
-    margin: 0;
-    background-color: #0c0c0c;
-`
-const FooterContent = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 0;
-    margin: 0;
-    align-items: center;
-    img:nth-child(1) {
-        width: 10em;
-    }
-    img:nth-child(2) {
-        width: 10em;
-    } 
-    @media (min-width: 1100px) {
-        img:nth-child(1) {
-            width: 200px;
-        }
-        img:nth-child(2) {
-            width: 200px;
-        } 
-    }
-`
+import './footer.scss'
 
 
 function Footer(){
     return(
         <>
-            <FooterContainer>
-                <FooterContent>
+            <div className="FooterContainer">
+                <div className="FooterContent">
                     <img
                         src={logo}
                         alt="GaloIcon"></img>
                     <img 
                         src={fiap}
                         alt="FiapLogo"></img>
-                </FooterContent>
-            </FooterContainer>
+                </div>
+            </div>
         </>
     )
 }
