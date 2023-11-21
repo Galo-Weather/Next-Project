927e4261b1e2f21742a3431986b5b44ee901dbcc
Akiozin <rm98582@fiap.com.br>
2023-11-21 12:43:46 -0300

    Sass file contato

--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -9,254 +9,7 @@ import Matheus from '../../imgs/time/Matheus.png'
 import Vinicius from '../../imgs/time/Vinicius.png'
 import WppIcon from "../../imgs/icons/whatsapp-icon.png";
 import EmailIcon from "../../imgs/icons/email-icon.png";
-
-const AboutContainer = styled.section`
-background: linear-gradient(#e8e8e8, #ffffff 15%);
-  text-align: center;
-  padding: 1em;
-  h1{
-    font-size: 3em;
-    font-weight: bold;
-    margin: 1em 0;
-    color: #0f62fe;
-  }
-
-  @media (min-width: 1100px) {
-    margin-top: 5.5em;
-    display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  text-align: center;
-  h1{
-    margin-bottom: 1em;
-  }
-}
-`;
-
-const Container = styled.div`
-
-  padding: 1em;
-  border-radius: 30px;
-  box-shadow: 0 1em 1em rgba(214, 214, 214, 0.8);
-  
-
-  @media (min-width: 1100px) {
-    display: flex;
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-  
-`;
-
-const AboutImg = styled.div`
-img{
-    width: 100%;
-    padding: 1em;
-}
-`;
-
-const AboutTxT = styled.div`
-  max-width: 600px;
-  padding-top: 1em;
-  font-weight: 400;
-`;
-
-const CardContainer = styled.div`
-display:flex;
-justify-content: center;
-align-items: center;
-margin-top: 50px;
-flex-wrap: wrap;
-`
-
-const Card = styled.div`
-width: 325px;
-background-color: #0f62fe;
-color: #fff;
-border-radius: 10px;
-box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-overflow: hidden;
-margin: 10px;
-img{
-    width:100%;
-    height: 225px;
-    object-fit: cover;
-}
-`
-
-const CardContent = styled.div`
-padding: 20px;
-h2{
-    font-size: 24px;
-    margin-bottom: 10px;
-}
-p{
-    font-size: 14px;
-    margin-bottom: 20px;
-}
-`
-const ContatoContainer = styled.section`
-background: linear-gradient(#ffffff, #e8e8e8 15%);
-align-items: center;
-padding: 1em;
-
-@media (min-width: 1100px) {
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    padding: 1em;
-}
-`
-const InnerContatoContainer = styled.div`
-`
-const ContatoContainerHeader = styled.div`
-    text-align: center;
-    p:first-child {
-        font-weight: bold;
-        font-size: 3em;
-        color: #0f62fe;
-    }
-
-@media (min-width: 1100px) {
-    text-align: center;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    p:first-child {
-        font-weight: 800;
-        font-size: 4em;
-        color: #0f62fe;
-    }
-    p:nth-child(2) {
-        font-weight: 800;
-        font-size: 1.4em;
-        color: #00000;
-        margin-bottom: 30px
-    }
-}
-`
-const CardsContainer = styled.div`
-margin-top: 1em;
-
-@media (min-width: 1100px) {
-    display: flex;
-    gap: 7em;
-}
-`
-const SobreCardContainer = styled.div`
-    border-radius: 30px;
-    background: #fff;
-    box-shadow: 0 1em 1em rgba(214, 214, 214, 0.8);
-    text-align: center;
-    padding: 1em;
-    margin-bottom: 1em;
-
-@media (min-width: 1100px) {
-    width: 367px;
-    height: 400px;
-    border-radius: 30px;
-    background: #fff;
-    box-shadow: 0 1em 1em rgba(214, 214, 214, 0.8);
-    display: flex;
-    justify-content: center;
-    padding: 1em 0 0 0;
-    flex-wrap: wrap;
-}
-`
-const SobreCardImgContainer = styled.figure`
-
-    padding: 1em;
-    img{
-        max-width: 50%;
-    }
-
-@media (min-width: 1100px) {
-    flex-basis: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 65px;
-    img{
-        width: 20%;
-    }
-}
-`
-const CardTextContainer = styled.div`
-
-text-align: center;
-color: #000000;
-font-size: 1em;
-padding: 1em;
-p:first-child {
-    font-size: 1.5em;
-    font-weight: 800;
-    color: #0f62fe;
-    padding: 5px
-}
-button {
-    background-color: #D9D9D9;
-    border: none;
-    height: 50px;
-    width: 200px;
-    text-align: center;
-    -webkit-transition-duration: 0.4s;
-    transition-duration: 0.4s;
-    text-decoration: none;
-    overflow: hidden;
-    color: #757373;
-    font-size: 1em;
-    font-weight: 700;
-    border-radius: 2em;
-}
-button:hover {
-    background: #0f62fe;
-    box-shadow: 0px 2px 10px 5px #97B1BF;
-    color: #fff;
-}
-
-@media (min-width: 1100px) {
-    text-align: center;
-    color: #000000;
-    font-size: 1em;
-    display: inline-block;
-    p:first-child {
-        font-size: 1.5em;
-        font-weight: 800;
-        color: #0f62fe;
-        margin: 5px
-    }
-    button {
-        background-color: #D9D9D9;
-        border: none;
-        height: 50px;
-        width: 200px;
-        text-align: center;
-        -webkit-transition-duration: 0.4s;
-        transition-duration: 0.4s;
-        text-decoration: none;
-        overflow: hidden;
-        color: #757373;
-        font-size: 1em;
-        font-weight: 700;
-        border-radius: 2em;
-    }
-    button:hover {
-        background: #0f62fe;
-        box-shadow: 0px 2px 10px 5px #97B1BF;
-        color: #fff;
-    }
-    button:active:after {
-        padding: 0;
-        margin: 0;
-        opacity: 1;
-        transition: 0s
-      }
-}
-`
+import './contato.scss'
 
 function Contato() {
 
@@ -271,16 +24,16 @@ function Contato() {
     return (
         <>
             <Element name='Sobre'>
-                <AboutContainer>
+                <div className="AboutContainer">
                     <h1>Sobre nós</h1>
-                    <Container>
-                        <AboutImg>
+                    <div className="Container">
+                        <div className="AboutImg">
                             <img
                                 src={Galoicon}
                                 alt="Icone da logo marca">
                             </img>
-                        </AboutImg>
-                        <AboutTxT>
+                        </div>
+                        <div className="AboutTxT">
                             <p> No coração do "Galo Weather" está nossa visão ousada de transformar radicalmente a forma como
                                 enfrentamos os desafios dos alagamentos urbanos. Inspirados pelo galo, símbolo tradicionalmente
                                 associado à previsão do tempo e à vigilância, buscamos antecipar e mitigar as inundações que impactam
@@ -293,109 +46,109 @@ function Contato() {
                                 nós, no "Galo Weather", aspiramos a ser a voz que alerta e mobiliza comunidades para se prepararem e se protegerem.
                                 Nossa missão é utilizar a sabedoria da natureza, combinada com a inovação tecnológica, para forjar um futuro onde
                                 as cidades possam enfrentar os desafios climáticos, garantindo segurança e bem-estar para todos.</p>
-                        </AboutTxT>
-                    </Container>
-                </AboutContainer>
+                        </div>
+                    </div>
+                </div>
 
-                <CardContainer>
+                <div className="CardContainer">
 
-                    <Card>
+                    <div className="Card">
                         <img
                             src={Fabricio}
                             alt="Integrante do grupo Galo">
                         </img>
-                        <CardContent>
+                        <div className="CardContent">
                             <h2>Fabrício Saavedra</h2>
                             <p>RM 97631</p>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
 
-                    <Card>
+                    <div className="Card">
                         <img
                             src={GuilhermeA}
                             alt="Integrante do grupo Galo">
                         </img>
-                        <CardContent>
+                        <div className="CardContent">
                             <h2>Guilherme Akio</h2>
                             <p>RM 98582</p>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
 
-                    <Card>
+                    <div className="Card">
                         <img
                             src={GuilhermeM}
                             alt="Integrante do grupo Galo">
                         </img>
-                        <CardContent>
+                        <div className="CardContent">
                             <h2>Guilherme Morais</h2>
                             <p>RM 551981</p>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
 
-                    <Card>
+                    <div className="Card">
                         <img
                             src={Matheus}
                             alt="Integrante do grupo Galo">
                         </img>
-                        <CardContent>
+                        <div className="CardContent">
                             <h2>Matheus Motta</h2>
                             <p>RM 550352</p>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
 
-                    <Card>
+                    <div className="Card">
                         <img
                             src={Vinicius}
                             alt="Integrante do grupo Galo">
                         </img>
-                        <CardContent>
+                        <div className="CardContent">
                             <h2>Vinicius Buzato</h2>
                             <p>RM 99125</p>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
 
-                </CardContainer>
+                </div>
 
             </Element>
             <Element name='Contato'>
-                <ContatoContainer>
-                    <InnerContatoContainer>
-                        <ContatoContainerHeader>
+                <div className="ContatoContainer">
+                    <div className="InnerContatoContainer">
+                        <div className="ContatoContainerHeader">
                             <p>Entre em contato</p>
                             <p>Nós adoraríamos ouvir você. Aqui como você pode chegar até nós</p>
-                        </ContatoContainerHeader>
-                        <CardsContainer>
-                            <SobreCardContainer>
-                                <SobreCardImgContainer>
+                        </div>
+                        <div className="CardsContainer">
+                            <div className="SobreCardContainer">
+                                <div className="SobreCardImgContainer">
                                     <img
                                         src={WppIcon}
                                         alt="Ícone de whatsApp">
                                     </img>
-                                </SobreCardImgContainer>
-                                <CardTextContainer>
+                                </div>
+                                <div className="CardTextContainer">
                                     <p>Contato via WhatsApp</p>
                                     <p>Em caso de dúvidas, entre em contato!</p>
                                     <p>Nosso atendimento é das 08:00 as 18:00, com respostas em até 24 horas durante dias úteis</p>
                                     <button onClick={sendWppClick}>Clique aqui</button>
-                                </CardTextContainer>
-                            </SobreCardContainer>
-                            <SobreCardContainer>
-                                <SobreCardImgContainer>
+                                </div>
+                            </div>
+                            <div className="SobreCardContainer">
+                                <div className="SobreCardImgContainer">
                                     <img
                                         src={EmailIcon}
                                         alt="Ícone de email">
                                     </img>
-                                </SobreCardImgContainer>
-                                <CardTextContainer>
+                                </div>
+                                <div className="CardTextContainer">
                                     <p>Contato via Email</p>
                                     <p>Em caso de dúvidas, entre em contato!</p>
                                     <p>Seu email será respondidoem até 24 horas durante dias úteis</p>
                                     <button onClick={handleEmailClick}>Clique aqui</button>
-                                </CardTextContainer>
-                            </SobreCardContainer>
-                        </CardsContainer>
-                    </InnerContatoContainer>
-                </ContatoContainer>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Element>
         </>
     );
